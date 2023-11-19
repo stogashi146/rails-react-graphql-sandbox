0f714ac4b310bc40c5520549d97b83c60eb3d70a
stogashi146 <stogashi146@gmail.com>
2023-11-19 12:14:10 +0900

    Bookをすべて取得、1件取得するqueryを作成

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -22,10 +22,13 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
+    # field :test_field, String, null: false,
+    #   description: "An example field added by the generator"
+    # def test_field
+    #   "Hello World!"
+    # end
+    field :books, resolver: Queries::Books
+    field :book, resolver: Queries::Book
+
   end
 end
